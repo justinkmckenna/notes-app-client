9ecd738a92a0ec878017d629f2df38f2c0c1b91b
Justin Mckena <mckennaman123@gmail.com>
2020-08-10 13:46:07 -0400

    custom form fields hook

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -6,27 +6,31 @@ import { observer } from "mobx-react";
 import { AuthenticatedStoreContext } from "../Stores/AuthenticatedStore";
 import { useHistory } from "react-router-dom";
 import LoaderButton from "./LoaderButton";
+import { onError } from "../Libs/errorLib";
+import { useFormFields } from "../Libs/hooksLib";
 
 export const Login = observer(() => {
   const history = useHistory();
   const authenticatedStore = useContext(AuthenticatedStoreContext);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: ""
+  });
   const [isLoading, setIsLoading] = useState(false);
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return fields.email.length > 0 && fields.password.length > 0;
   }
 
   async function handleSubmit(event: { preventDefault: () => void; }) {
     event.preventDefault();
     setIsLoading(true);
     try {
-      await Auth.signIn(email, password);
+      await Auth.signIn(fields.email, fields.password);
       authenticatedStore.authenticated = true;
       history.push("/");
     } catch (e) {
-      alert(e.message);
+      onError(e);
       setIsLoading(false);
     }
   }
@@ -39,15 +43,15 @@ export const Login = observer(() => {
           <FormControl
             autoFocus
             type="email"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
+            value={fields.email}
+            onChange={handleFieldChange}
           />
         </FormGroup>
         <FormGroup controlId="password">
           <FormLabel>Password</FormLabel>
           <FormControl
-            value={password}
-            onChange={e => setPassword(e.target.value)}
+            value={fields.password}
+            onChange={handleFieldChange}
             type="password"
           />
         </FormGroup>
