81563aa4e791b954f0cd4333a0dd1dcd6cf25b76
Justin Mckena <mckennaman123@gmail.com>
2020-08-13 12:11:15 -0400

    dev and prod envs

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,31 @@
-export default {
+  const dev = {
     apiGateway: {
       REGION: "us-east-1",
-      URL: "https://iylp03s2ei.execute-api.us-east-1.amazonaws.com/prod"
+      URL: "https://ejav5kt3ta.execute-api.us-east-1.amazonaws.com/dev"
     },
     cognito: {
       REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_4Cat7VgV5",
-      APP_CLIENT_ID: "6ca60fqsujad76lgbnskebm94l",
-      IDENTITY_POOL_ID: "us-east-1:922fbb54-f2d0-47a3-8379-8ae35830f7b6"
+      USER_POOL_ID: "us-east-1_rXepvcswV",
+      APP_CLIENT_ID: "2m5l03gc8irqqogvdtuml2sbjo",
+      IDENTITY_POOL_ID: "us-east-1:18491440-3333-4688-9eaa-dcd10b99ef9f"
     }
   };
+  
+  const prod = {
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://33h2shws89.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_pX6CkWdCX",
+      APP_CLIENT_ID: "4u9ipoi4sta2j9trvhrl17hj4m",
+      IDENTITY_POOL_ID: "us-east-1:605d7391-8482-49f0-bdc4-33b912afc106"
+    }
+  };
+  
+  const config = process.env.REACT_APP_STAGE === 'production'
+    ? prod
+    : dev;
+  
+  export default config;
