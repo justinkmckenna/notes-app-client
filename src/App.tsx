0c8a4ed4fb6952bc0844bda72735e341984161bd
Justin Mckena <mckennaman123@gmail.com>
2020-08-14 16:34:24 -0400

    new favicon and options for notification

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,18 @@ const App = observer(() => {
   }, []);
 
   function displayNotification() {
-    if (Notification.permission == 'granted') {
+    if (Notification.permission == 'granted' && config.env == "prod") {
       navigator.serviceWorker.getRegistration().then(function(reg) {
-        reg!.showNotification('Hello world!');
+        var options = {
+          body: 'Here is a notification body!',
+          icon: 'public/favicon.ico',
+          vibrate: [100, 50, 100],
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+          }
+        };
+        reg!.showNotification('Hello world!', options);
       });
     }
   }
